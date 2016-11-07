ec89ccd396b8a9f267783fcc8c8fc2b1ac60a19d
Eduardo Osorio <eduardo@ebpo.com.br>
2016-11-07 14:56:45 -0200

    trying to remove sidebar

--- wp-content/themes/wp-portfolio/page-landing.php
+++ wp-content/themes/wp-portfolio/page-landing.php
@@ -1,15 +1,13 @@
 
 <?php get_header($landing);
-// do_action('wp_portfolio_before_primary');
+/* do_action('wp_portfolio_before_primary'); */
 ?>
 <div id="primary landing">
 <?php
 
-// do_action('wp_portfolio_before_loop_content');
 
 do_action('wp_portfolio_loop_content');
 
-// do_action('wp_portfolio_after_loop_content');
 ?>
 </div><!-- #primary -->
 </div><!-- #content -->
@@ -19,7 +17,4 @@ do_action('wp_portfolio_loop_content');
  */
 do_action('wp_portfolio_after_primary');
 ?>
-<!-- <div id="secondary">
-	<?php get_sidebar(); ?>
-</div> -->
 <?php get_footer(); ?>
