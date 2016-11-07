f8a0111c9d0a08cf7f0236b414eb3af77b46792e
Eduardo Osorio <eduardo@ebpo.com.br>
2016-11-07 14:51:34 -0200

    trying to remove sidebar

--- wp-content/themes/wp-portfolio/inc/structure/content-extensions.php
+++ wp-content/themes/wp-portfolio/inc/structure/content-extensions.php
@@ -30,7 +30,7 @@ add_action( 'wp_portfolio_loop_content', 'wp_portfolio_theloop', 10 );
  * Shows the loop content
  */
 function wp_portfolio_theloop() {
-	if( is_page($page = 'landing') ) {
+	if( is_page( 'landing' ) ) {
 		wp_portfolio_theloop_for_landing();
 	}
 	elseif( is_page() ) {
