dcb78e4add1027ee1ff00fa7c1f4ce9afd48cf5e
Eduardo Osorio <eduardo@ebpo.com.br>
2016-11-07 14:38:21 -0200

    trying to remove sidebar

--- wp-content/themes/wp-portfolio/header-landing.php
+++ wp-content/themes/wp-portfolio/header-landing.php
@@ -12,13 +12,6 @@
 <head>
 <meta charset="<?php bloginfo('charset');?>" />
 	<?php
-	/**
-	 * wp_portfolio_view_port hook
-	 *
-	 * HOOKED_FUNCTION_NAME PRIORITY
-	 *
-	 *
-	 */
 	do_action('wp_portfolio_view_port'); ?>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url');?>" />
@@ -27,12 +20,5 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<?php
-				/**
-				 * wp_portfolio_header hook
-				 *
-				 * HOOKED_FUNCTION_NAME PRIORITY
-				 *
-				 * wp_portfolio_headercontent_details 10
-				 */
 				// do_action('wp_portfolio_header');
 			?>
